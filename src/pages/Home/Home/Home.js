17f034ef0fb78135c3681891f9480f9000e243bb
zohirulislam1 <codershahin84@gmail.com>
2022-05-11 10:00:54 +0600

    footer page

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Appoienment from '../Appoientment/Appoienment';
 import Bannar from '../Bannar/Bannar';
 import Contract from '../Contract/Contract';
+import Footer from '../Footer/Footer';
 import Info from '../Info/Info';
 import Reviews from '../Reviews/Reviews';
 import Services from '../Services/Services';
@@ -15,6 +16,7 @@ const Home = () => {
             <Appoienment></Appoienment>
             <Reviews></Reviews>
             <Contract></Contract>
+            <Footer></Footer>
         </div>
     );
 };
